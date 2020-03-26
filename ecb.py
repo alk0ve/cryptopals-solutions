5f013986abfcff87cd75ed43970baf6c0e5355ed
Alexei Kovelman <alexei.kovelman@gmail.com>
2020-03-26 13:40:18 +0200

    Solve 1.8 (ecb.py)

--- ecb.py
+++ ecb.py
@@ -3,15 +3,24 @@
 from Crypto.Cipher import AES
 import sys
 import base64
+import binascii
 
+AES_BLOCK_SIZE = 16
 
 def main():
     assert len(sys.argv) == 2
     with open(sys.argv[1], 'r') as f:
-        encrypted = base64.b64decode(f.read().replace('\n',''))
-    
-    aes_ecb = AES.new("YELLOW SUBMARINE", AES.MODE_ECB)
-    print(aes_ecb.decrypt(encrypted))
+        encrypted_lines = [binascii.unhexlify(line) for line in f.read().split()]
+    for line_num, line in enumerate(encrypted_lines):
+        encrypted_blocks = set()
+        for offset in range(0, len(line), AES_BLOCK_SIZE):
+            block = line[offset: offset + AES_BLOCK_SIZE]
+            if block in encrypted_blocks:
+                # bingo
+                print("Found a repeat in line %d: %s" % (line_num, binascii.hexlify(block)))
+                return
+            encrypted_blocks.add(block)
+            
 
 if __name__ == "__main__":
     main()
